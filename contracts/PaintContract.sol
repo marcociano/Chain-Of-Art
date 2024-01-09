03ee916947e99e8e3bc6edbdcb8d811acf6f80e8
Marco Ciano <marco.ciano@live.it>
2024-01-09 22:54:05 +0100

    Transaction bug fix

--- contracts/PaintContract.sol
+++ contracts/PaintContract.sol
@@ -3,42 +3,75 @@ pragma solidity ^0.8.0;
 
 contract PaintContract {
     address public owner;
-    mapping(uint256 => address) public paintOwners;
-    mapping(uint256 => bool) public paintPurchased;
-    //Mapping per convertire i prezzi
+
+    struct Purchase {
+        address buyer;
+        bool isPurchased;
+    }
+
+    uint256 public paintPurchasesCount;
+    mapping(uint256 => Purchase) public paintPurchases;
     mapping(uint256 => uint256) public paintPrices;
 
     event PaintPurchased(address indexed buyer, uint256 indexed paintId);
 
     modifier onlyOwner() {
-       require(msg.sender == owner, "Only the owner can call this function");
+        require(msg.sender == owner, "Only the owner can call this function");
         _;
     }
 
     constructor() {
         owner = msg.sender;
+        paintPurchasesCount = 0;
     }
 
-    //Imposta il prezzo
-    function setPaintPrice(uint256 paintId, uint256 price) external onlyOwner {
-        paintPrices[paintId] = price;
+    function setPaintPrice(uint256 paintId) external onlyOwner payable {
+    require(msg.value > 0, "No Ether sent");
+    paintPrices[paintId] = msg.value;
+}
+
+
+  function purchasePaint(uint256 paintId) external payable {
+    require(!paintPurchases[paintId].isPurchased, "Paint already purchased");
+    require(msg.value >= paintPrices[paintId], "Not enough Ether sent");
+
+    // Calcola la quantità di Ether effettivamente pagata per il quadro
+    uint256 actualPrice = paintPrices[paintId];
+    require(msg.value >= actualPrice, "Not enough Ether sent");
+
+    // Effettua la transazione utilizzando l'actualPrice
+    (bool success, ) = owner.call{value: actualPrice}("");
+    require(success, "Transaction failed");
+
+    // Aggiorna lo stato del quadro
+    paintPurchases[paintId] = Purchase(msg.sender, true);
+    paintPurchasesCount++;
+
+    emit PaintPurchased(msg.sender, paintId);
+}
+
+
+    function getAcquisitionsByBuyer(address buyer) external view returns (uint256[] memory) {
+        uint256 count = 0;
+
+        // Conta il numero di quadri acquistati dall'acquirente specificato
+        for (uint256 i = 0; i < paintPurchasesCount; i++) {
+            if (paintPurchases[i].buyer == buyer && paintPurchases[i].isPurchased) {
+                count++;
+            }
+        }
+
+        // Crea un array contenente gli ID dei quadri acquistati
+        uint256[] memory result = new uint256[](count);
+        count = 0;
+        for (uint256 i = 0; i < paintPurchasesCount; i++) {
+            if (paintPurchases[i].buyer == buyer && paintPurchases[i].isPurchased) {
+                result[count] = i;
+                count++;
+            }
+        }
+
+        return result;
     }
-
-    function purchasePaint(uint256 paintId) external payable {
-        require(!paintPurchased[paintId], "Paint already purchased");
-        require(msg.value >= paintPrices[paintId], "Not enough Ether sent");
-
-        paintOwners[paintId] = msg.sender;
-        paintPurchased[paintId] = true;
-
-        emit PaintPurchased(msg.sender, paintId);
-    }
-
-    function getPaint(uint256 paintId) external view returns (address) {
-        return paintOwners[paintId];
-    }
-
-    function setOwner(address newOwner) external onlyOwner {
-        owner = newOwner;
-    }
+   
 }
--- src/js/app.js
+++ src/js/app.js
@@ -10,7 +10,6 @@ App = {
 
             for (i = 0; i < data.length; i++) {
                 var newCard = paintsTemplate.find('.card_box').clone();
-
                 newCard.find('.paint-title').text(data[i].nome);
                 newCard.find('.paint-image').attr('src', data[i].immagine);
                 newCard.find('.artist-name').text(data[i].artista);
@@ -20,7 +19,8 @@ App = {
                 paintsRow.append(newCard);
             }
         });
-        return await App.initWeb3();
+        return App.initWeb3();
+        
     },
 
     initWeb3: async function () {
@@ -44,7 +44,7 @@ App = {
             App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
         }
         web3 = new Web3(App.web3Provider);
-
+        
         return App.initContract();
     },
 
@@ -56,66 +56,103 @@ App = {
 
             // Set the provider for our contract
             App.contracts.PaintContract.setProvider(App.web3Provider);
-
+           
             return App.markPurchased();
         });
 
         return App.bindEvents();
     },
 
-
+    setPaintPrices: function (paintInstance) {
+        // Imposta i prezzi dei quadri
+        try {
+            var data = $.getJSON('paints.json');
+            for (var i = 0; i < data.length; i++) {
+                var price = data[i].prezzo; 
+                paintInstance.setPaintPrice(i, web3.utils.toWei(price.toString(), 'ether'), { from: account });
+            }
+        } catch (err) {
+            console.log(err.message);
+        }
+    },
+    
+    
 
     bindEvents: function () {
-        $('.paintsRow').on('click', '.btn-purchase', App.handlePurchase);
+        $(document).on('click', '.btn-purchase', App.handlePurchase);
     },
 
     // Handle paint purchase
     markPurchased: function () {
-        var paintInstance;
-
-        App.contracts.PaintContract.deployed().then(function (instance) {
-            paintInstance = instance;
-            return paintInstance.getPaint.call();
-        }).then(function (buyer) {
-            for (i = 0; i < buyer.length; i++) {
-                if (buyer[i] !== '0x0000000000000000000000000000000000000000') {
-                    $('.card_box').eq(i).find('button').text('Acquistato').attr('disabled', true);
+        try {
+            var paintInstance = App.contracts.PaintContract.deployed();
+            var buyer = paintInstance.getPaints.call();
+    
+            // Verifica che l'array buyer non sia vuoto
+            if (buyer && buyer.length > 0) {
+                for (var i = 0; i < buyer.length; i++) {
+                    if (buyer[i] !== '0x0000000000000000000000000000000000000000') {
+                        $('.card_box').eq(i).find('button').text('Acquista').attr('disabled', true);
+                        App.updatePurchasedTable(i);
+                    }
                 }
             }
-        }).catch(function (err) {
+        } catch (err) {
             console.log(err.message);
-        });
+        }
     },
-
+    
+    updatePurchasedTable: function (paintId) {
+        try {
+            var paintInstance = App.contracts.PaintContract.deployed();
+            var paint = paintInstance.getPaint(paintId);
+    
+            // Aggiorna la tabella con le informazioni del quadro acquistato
+            var purchasedTable = $('#paintPurchased');
+            var newRow = '<tr><th scope="row">' + paintId +
+                '</th><td><img src="' + $('.card_box').eq(paintId).find('.paint-image').attr('src') +
+                '" class="img-fluid w-50"></td><td>' + paint.title +
+                '</td><td>' + paint.price +
+                '</td><td>' + paint.artist +
+                '</td><td>Acquistato</td></tr>';
+    
+            purchasedTable.append(newRow);
+        } catch (err) {
+            console.log(err.message);
+        }
+    },
+    
 
     handlePurchase: function (event) {
         event.preventDefault();
-
+    
         var paintId = parseInt($(event.target).data('id'));
-
+    
         var paintInstance;
-
+    
         web3.eth.getAccounts(function (error, accounts) {
             if (error) {
-                console.log(error);
+                console.error(error);
             }
-
+    
             var account = accounts[0];
             App.contracts.PaintContract.deployed().then(function (instance) {
                 paintInstance = instance;
-
+    
                 // Recupera il prezzo del quadro
-                return paintInstance.paintPrices(paintId).call();
+                return paintInstance.paintPrices.call(paintId);
             }).then(function (paintPrice) {
                 // Esegui la funzione di acquisto con il prezzo corretto
                 return paintInstance.purchasePaint(paintId, { from: account, value: paintPrice });
             }).then(function (result) {
-                return App.markPurchased(paintId);  // Passa l'ID del quadro
+                alert("Quadro Acquistato");
+                window.location.reload();
             }).catch(function (err) {
-                console.log(err.message);
+                console.error(err.message);
             });
         });
     }
+      
 };
 
 
