6b20d4970843b35b8a35f1c71afeacb75d67e153
Marco Ciano <marco.ciano@live.it>
2024-01-14 17:15:45 +0100

    Progetto v1.0 stabile

--- contracts/PaintContract.sol
+++ contracts/PaintContract.sol
@@ -1,45 +1,56 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-/*All'interno di questo smart contract sono inseriti i seguenti design pattern:
- * n°1 Checks-Effects-Interactions Pattern -> stabilisce la sequenza delle operazioni secondo l'ordine check(attuare controlli per verificare le condizioni necessarie), effects (applicazione), interactions(gestite le interazioni).
- * n°2 Owner Pattern -> Garantisce che solo il proprietario esegua operazioni "critiche".
- * n°3 Emergency Stop -> Sospende temporaneamente l'esecuzione in caso di emergenza.
- * n°4 Reentrancy Guard Pattern -> Protegge dagli attacchi di reentrancy.
- * n°5 Time-Dependent Restrictions Pattern -> Consente di applicare restrizioni basate sul tempo.
- */
 
-
+// Contratto per la gestione di una collezione di quadri digitali
 contract PaintContract {
     uint public counterPaints;
+
+    // Struttura di un singolo quadro
     struct Paint {
         uint id;
         string title;
         string img;
         string artist;
-        uint price;
+        string price;
         bool status;
     }
 
+    // Mappatura degli ID dei quadri con i loro dettagli
     mapping (uint => Paint) public paints;
-    mapping (address => uint[]) public purchasedPaints;
-  
 
+    // Mappatura degli indirizzi degli acquirenti con gli ID dei quadri acquistati
+    mapping (address => uint[]) public purchasedPaints;
+
+    // Evento emesso quando un quadro viene acquistato
     event purchasedPaint(address indexed buyer, uint indexed paintId);
 
+    // Costruttore del contratto
     constructor() {
-       counterPaints = 0;
-        //Inizializzazione del Time-Dependent Restrictions Pattern
-        //contractStartTime = block.timestamp + 1 days; 
+        counterPaints = 0;
+        loadPaints();
     }
 
+    function loadPaints() public {
+        createPaint(0,"Distese Stellate", "img/ai1.jpeg", "Alessandro Vibrante", "0.33");
+        createPaint(1,"Sinfonia di Spirali Cosmiche", "img/img_ia_1.png", "Marco Tessitore", "0.43");
+        createPaint(2,"Il giorno", "img/ai2.jpeg", "Vittorio Formale", "0.26");
+        createPaint(3,"Equilibrio Astratto", "img/ai3.jpeg", "Silvio Baratta", "0.47");
+        createPaint(4,"L'Eden", "img/ai4.jpeg", "Gioele Patternato", "0.22");
+        createPaint(5,"Alba dorata nel paese", "img/img_ia_2.png", "Leonardo Luminoso", "0.14");
+        createPaint(6,"Tempesta Solare sulle Cime Eterne", "img/img_ia_3.png", "Flavio Sogno", "0.15");
+        createPaint(7,"Allegria del Villaggio", "img/ai5.jpeg", "Bruno Elementare", "0.47");
+        createPaint(8,"Liberta'", "img/ai6.jpeg", "Angelo Romano", "0.47");
+        createPaint(9,"Fioritura del Sentimento", "img/ai7.jpeg", "Francesco Prete", "0.41");  
+        createPaint(10,"Il volo", "img/ai8.jpeg", "Luca Cecchino", "0.37");
+    }
 
-
+    // Funzione per creare un nuovo quadro nella collezione
     function createPaint(
         uint id,
         string memory title,
         string memory img,
         string memory artist,
-        uint price 
+        string memory price 
     ) public {
         require(paints[id].id == 0, "Quadro con lo stesso ID gia' esistente");
 
@@ -54,12 +65,13 @@ contract PaintContract {
         counterPaints++;
     }
 
+    // Funzione per ottenere i dettagli di un quadro dato il suo ID
     function getPaintDetails(uint paintId) public view returns (
         uint id,
         string memory title,
         string memory img,
         string memory artist,
-        uint price,
+        string memory price,
         bool status 
     ){
         Paint storage paint = paints[paintId];
@@ -73,20 +85,30 @@ contract PaintContract {
         );
     }
 
+    // Funzione per acquistare un quadro
     function buyPaint(uint paintId) public payable {
         Paint storage paint = paints[paintId];
+
+        // Verifica se il quadro è disponibile
         require(paint.status, "Il quadro non e' disponibile");
 
-        require(!purchasedSatus(msg.sender, paintId), "Hai gia' comprato questo quadro");
+        // Verifica se l'utente ha già acquistato questo quadro
+        require(!purchasedStatus(msg.sender, paintId), "Hai gia' comprato questo quadro");
 
+        // Aggiungi l'ID del quadro agli acquisti dell'utente
         purchasedPaints[msg.sender].push(paintId);
 
+        // Imposta lo stato del quadro come non disponibile
+        paint.status = false;
+        
+        // Emetti l'evento per registrare l'acquisto
         emit purchasedPaint(msg.sender, paintId);
     }
 
-    function purchasedSatus(address buyer, uint paintId) internal view returns (bool) {
+    // Funzione di supporto per verificare se un utente ha già acquistato un quadro
+    function purchasedStatus(address buyer, uint paintId) internal view returns (bool) {
         uint[] storage purchasedIds = purchasedPaints[buyer];
-        for (uint i = 0; i< purchasedIds.length; i++){
+        for (uint i = 0; i < purchasedIds.length; i++) {
             if (purchasedIds[i] == paintId) {
                 return true;
             }
@@ -94,19 +116,13 @@ contract PaintContract {
         return false;
     }
 
-    function getAllPaintd() public view returns (Paint[] memory){
-        Paint[] memory result = new Paint[](counterPaints);
-        for(uint i = 0; i< counterPaints; i++){
-            result[i] = paints[i];
-        }
-        return result;
-    }
-
+    // Funzione per ottenere gli ID dei quadri acquistati da un utente
     function getPurchasedPaintIds(address buyer) public view returns (uint[] memory){
         uint[] memory purchasedIds = purchasedPaints[buyer];
         return purchasedIds;
     }
 
+    // Funzione per ottenere i dettagli dei quadri acquistati da un utente
     function getPurchasedPaints(address buyer) public view returns (Paint[] memory){
         uint[] memory purchasedIds = purchasedPaints[buyer];
         Paint[] memory purchasedPaintsArray = new Paint[](purchasedIds.length);
@@ -116,5 +132,4 @@ contract PaintContract {
         }
         return purchasedPaintsArray;
     }      
-    
 }
