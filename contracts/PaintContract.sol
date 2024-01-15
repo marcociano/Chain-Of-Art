a2e9f2dfebf244843ca3fe665d3a54fd8a4e8d30
Marco Ciano <marco.ciano@live.it>
2024-01-15 17:22:18 +0100

    Chain-Of-Art v1.0.2 final

--- contracts/PaintContract.sol
+++ contracts/PaintContract.sol
@@ -16,10 +16,10 @@ contract PaintContract {
     }
 
     // Mappatura degli ID dei quadri con i loro dettagli
-    mapping (uint => Paint) public paints;
+    mapping(uint => Paint) public paints;
 
     // Mappatura degli indirizzi degli acquirenti con gli ID dei quadri acquistati
-    mapping (address => uint[]) public purchasedPaints;
+    mapping(address => uint[]) public purchasedPaints;
 
     // Evento emesso quando un quadro viene acquistato
     event purchasedPaint(address indexed buyer, uint indexed paintId);
@@ -31,17 +31,59 @@ contract PaintContract {
     }
 
     function loadPaints() public {
-        createPaint(0,"Distese Stellate", "img/ai1.jpeg", "Alessandro Vibrante", "0.33");
-        createPaint(1,"Sinfonia di Spirali Cosmiche", "img/img_ia_1.png", "Marco Tessitore", "0.43");
-        createPaint(2,"Il giorno", "img/ai2.jpeg", "Vittorio Formale", "0.26");
-        createPaint(3,"Equilibrio Astratto", "img/ai3.jpeg", "Silvio Baratta", "0.47");
-        createPaint(4,"L'Eden", "img/ai4.jpeg", "Gioele Patternato", "0.22");
-        createPaint(5,"Alba dorata nel paese", "img/img_ia_2.png", "Leonardo Luminoso", "0.14");
-        createPaint(6,"Tempesta Solare sulle Cime Eterne", "img/img_ia_3.png", "Flavio Sogno", "0.15");
-        createPaint(7,"Allegria del Villaggio", "img/ai5.jpeg", "Bruno Elementare", "0.47");
-        createPaint(8,"Liberta'", "img/ai6.jpeg", "Angelo Romano", "0.47");
-        createPaint(9,"Fioritura del Sentimento", "img/ai7.jpeg", "Francesco Prete", "0.41");  
-        createPaint(10,"Il volo", "img/ai8.jpeg", "Luca Cecchino", "0.37");
+        createPaint(
+            0,
+            "Distese Stellate",
+            "img/ai1.jpeg",
+            "Alessandro Vibrante",
+            "0.33"
+        );
+        createPaint(
+            1,
+            "Sinfonia di Spirali Cosmiche",
+            "img/img_ia_1.png",
+            "Marco Tessitore",
+            "0.43"
+        );
+        createPaint(2, "Il giorno", "img/ai2.jpeg", "Vittorio Formale", "0.26");
+        createPaint(
+            3,
+            "Equilibrio Astratto",
+            "img/ai3.jpeg",
+            "Silvio Baratta",
+            "0.47"
+        );
+        createPaint(4, "L'Eden", "img/ai4.jpeg", "Gioele Patternato", "0.22");
+        createPaint(
+            5,
+            "Alba dorata nel paese",
+            "img/img_ia_2.png",
+            "Leonardo Luminoso",
+            "0.14"
+        );
+        createPaint(
+            6,
+            "Tempesta Solare sulle Cime Eterne",
+            "img/img_ia_3.png",
+            "Flavio Sogno",
+            "0.15"
+        );
+        createPaint(
+            7,
+            "Allegria del Villaggio",
+            "img/ai5.jpeg",
+            "Bruno Elementare",
+            "0.47"
+        );
+        createPaint(8, "Liberta'", "img/ai6.jpeg", "Angelo Romano", "0.47");
+        createPaint(
+            9,
+            "Fioritura del Sentimento",
+            "img/ai7.jpeg",
+            "Francesco Prete",
+            "0.41"
+        );
+        createPaint(10, "Il volo", "img/ai8.jpeg", "Luca Cecchino", "0.37");
     }
 
     // Funzione per creare un nuovo quadro nella collezione
@@ -50,30 +92,29 @@ contract PaintContract {
         string memory title,
         string memory img,
         string memory artist,
-        string memory price 
+        string memory price
     ) public {
         require(paints[id].id == 0, "Quadro con lo stesso ID gia' esistente");
 
-        paints[id] = Paint(
-            id,
-            title,
-            img,
-            artist,
-            price,
-            false
-        );
+        paints[id] = Paint(id, title, img, artist, price, false);
         counterPaints++;
     }
 
     // Funzione per ottenere i dettagli di un quadro dato il suo ID
-    function getPaintDetails(uint paintId) public view returns (
-        uint id,
-        string memory title,
-        string memory img,
-        string memory artist,
-        string memory price,
-        bool isSold 
-    ){
+    function getPaintDetails(
+        uint paintId
+    )
+        public
+        view
+        returns (
+            uint id,
+            string memory title,
+            string memory img,
+            string memory artist,
+            string memory price,
+            bool isSold
+        )
+    {
         Paint storage paint = paints[paintId];
         return (
             paint.id,
@@ -85,15 +126,26 @@ contract PaintContract {
         );
     }
 
+    function paintExists(uint paintId) public view returns (bool) {
+         return !paints[paintId].isSold;
+    }
+
     // Funzione per acquistare un quadro
     function buyPaint(uint paintId) public payable {
         Paint storage paint = paints[paintId];
+         require(paintExists(paintId), "Il quadro non e' disponibile");
 
         // Verifica se il quadro è disponibile
         require(!paint.isSold, "Il quadro non e' disponibile");
 
         // Verifica se l'utente ha già acquistato questo quadro
-        require(!purchasedStatus(msg.sender, paintId), "Hai gia' comprato questo quadro");
+        require(
+            !purchasedStatus(msg.sender, paintId),
+            "Il quadro non e' disponibile"
+        );
+
+        // Aggiorna lo stato del quadro come venduto
+        paint.isSold = true;
 
         // Aggiungi l'ID del quadro agli acquisti dell'utente
         purchasedPaints[msg.sender].push(paintId);
@@ -103,7 +155,10 @@ contract PaintContract {
     }
 
     // Funzione di supporto per verificare se un utente ha già acquistato un quadro
-    function purchasedStatus(address buyer, uint paintId) internal view returns (bool) {
+    function purchasedStatus(
+        address buyer,
+        uint paintId
+    ) internal view returns (bool) {
         uint[] storage purchasedIds = purchasedPaints[buyer];
         for (uint i = 0; i < purchasedIds.length; i++) {
             if (purchasedIds[i] == paintId) {
@@ -114,19 +169,23 @@ contract PaintContract {
     }
 
     // Funzione per ottenere gli ID dei quadri acquistati da un utente
-    function getPurchasedPaintIds(address buyer) public view returns (uint[] memory){
+    function getPurchasedPaintIds(
+        address buyer
+    ) public view returns (uint[] memory) {
         uint[] memory purchasedIds = purchasedPaints[buyer];
         return purchasedIds;
     }
 
     // Funzione per ottenere i dettagli dei quadri acquistati da un utente
-    function getPurchasedPaints(address buyer) public view returns (Paint[] memory){
+    function getPurchasedPaints(
+        address buyer
+    ) public view returns (Paint[] memory) {
         uint[] memory purchasedIds = purchasedPaints[buyer];
         Paint[] memory purchasedPaintsArray = new Paint[](purchasedIds.length);
 
-        for(uint i= 0; i< purchasedIds.length; i++){
+        for (uint i = 0; i < purchasedIds.length; i++) {
             purchasedPaintsArray[i] = paints[purchasedIds[i]];
         }
         return purchasedPaintsArray;
-    }      
+    }
 }
--- test/PaintContract.test.js
+++ test/PaintContract.test.js
@@ -1,61 +1,65 @@
+const truffleAssert = require('truffle-assertions');
 const PaintContract = artifacts.require("PaintContract");
 contract('PaintContract', (accounts) => {
-    let paintContract;
+  let paintContract;
 
-    before(async () => {
-        paintContract = await PaintContract.new({from: accounts[0]});
-    });
+  before(async () => {
+    paintContract = await PaintContract.new({ from: accounts[0] });
+  });
 
-    it('should create a new paint', async () => {
-        const paintId = 11;
-        const title = 'Test Paint';
-        const img = '/test.jpg';
-        const artist = 'Test Artist';
-        const price = '0.18';
+  it('should create a new paint', async () => {
+    const paintId = 11;
+    const title = 'Test Paint';
+    const img = '/test.jpg';
+    const artist = 'Test Artist';
+    const price = '0.18';
 
-        await paintContract.createPaint(paintId, title, img, artist, price);
+    await paintContract.createPaint(paintId, title, img, artist, price);
 
-        const paintDetails = await paintContract.getPaintDetails(paintId);
+    const paintDetails = await paintContract.getPaintDetails(paintId);
 
-        assert.equal(paintDetails.id, paintId, 'ID incorrect');
-        assert.equal(paintDetails.title, title, 'Title not set correctly');
-        assert.equal(paintDetails.img, img, 'Image not set correctly');
-        assert.equal(paintDetails.artist, artist, 'Artist not set correctly');
-        assert.equal(paintDetails.price, price, 'Price not set correctly');
-        assert.equal(paintDetails.isSold, false, 'isSold not set correctly');
-    });
+    assert.equal(paintDetails.id, paintId, 'ID incorrect');
+    assert.equal(paintDetails.title, title, 'Title not set correctly');
+    assert.equal(paintDetails.img, img, 'Image not set correctly');
+    assert.equal(paintDetails.artist, artist, 'Artist not set correctly');
+    assert.equal(paintDetails.price, price, 'Price not set correctly');
+    assert.equal(paintDetails.isSold, false, 'isSold not set correctly');
+  });
 
-    it('should allow buying a paint', async () => {
-        const paintId = 11;
-        const buyer = accounts[1];
-    
-        await paintContract.createPaint(paintId, "Test Paint", "Test Image", "Test Artist", "0.18");
-    
-        await paintContract.buyPaint(paintId, { from: buyer, value: web3.toWei('0.18', 'ether') });
-    
-        const purchasedPaints = await paintContract.getPurchasedPaints(buyer);
-    
-        assert.equal(purchasedPaints.length, 1, 'Paint not purchased successfully');
-    
-        const purchasedPaint = purchasedPaints[0];
-    
-        assert.equal(purchasedPaint.id, paintId, 'Paint Id is incorrect');
-    });
-    
+  it('should allow buying a paint', async () => {
+    const paintId = 11;
+    const buyer = accounts[1];
 
-    it('should prevent buying the same paint twice', async () => {
-        const paintId = 11;
+    // Acquista il quadro
+    await paintContract.buyPaint(paintId, { from: buyer, value: web3.utils.toWei('0.18', 'ether') });
 
-        await paintContract.createPaint(paintId, "Test Paint", "Test Image", "Test Artist", "0.18");
+    // Ottieni i dettagli del quadro
+    const paintDetails = await paintContract.getPaintDetails(paintId);
 
-        try {
-          await paintContract.createPaint(paintId, "Duplicate Paint", "Duplicate Image",  "Duplicate Artist", "0.18");
-          assert.fail("Should have thrown an error");
-        } catch (error) {
-          assert(error.message.includes("Quadro con lo stesso ID gia' esistente"), "Wrong error message");
-        }
-      });
-    
-    });
-    
-    
+    // Assicurati che il quadro sia contrassegnato come venduto
+    assert.equal(paintDetails.isSold, true, 'Il quadro è stato contrassegnato come venduto dopo l\' acquisto');
+
+    // Verifica che il quadro sia presente tra quelli acquistati dall'utente
+    const purchasedPaints = await paintContract.getPurchasedPaints(buyer);
+    assert.equal(purchasedPaints.length, 1, 'Il quadro non è stato acquistato con successo');
+    const purchasedPaint = purchasedPaints[0];
+    assert.equal(purchasedPaint.id, paintId, 'ID del quadro non corretto');
+  });
+
+
+  it('should prevent buying the same paint twice', async () => {
+    const paintId = 11;
+    const buyer = accounts[1];
+
+    // Tenta di acquistare lo stesso quadro di nuovo e cattura l'errore
+    try {
+      await paintContract.buyPaint(paintId, { from: buyer, value: web3.utils.toWei('0.18', 'ether') });
+      assert.fail("Dovrebbe generare un errore");
+    } catch (error) {
+      assert(error.message.includes("Il quadro non e' disponibile"), "Messaggio di errore errato");
+    }
+  });
+
+});
+
+
